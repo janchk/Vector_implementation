32f2266aadcea766837383fbf805d0f0e381ba8f
Jan Akhremchik <janchk@gmail.com>
2018-09-21 11:11:22 +0300

    some structure refactoring

--- src/task2/main.cc
+++ src/task2/main.cc
@@ -4,24 +4,70 @@ template <typename T>
 	class Vector{
 		private:
 			T *buffer;
-			unsigned int capacity;
+			size_type _size;
+			size_type _capacity;
+			T* data;
+			//unsigned int capacity;
+			
+			Static T*
+			allocate(size_type, size)
+			{
+				return static_cast<T*>(malloc(sizeof(T) * size));
+			}
+			static void
+			copyRange(T* begin, T* end, T* dest)
+			{
+				while(begin != end)
+				{
+					new((void*)dest) T(*begin);
+					++begin;
+					++dest;
+				}
+			}
+			static void
+			deleteRange(T* begin, T* end)
+			{
+				while(begin != end)
+				{
+				begin->~T();
+				++begin;
+				}
+			}
 		public:
-			Vector(){
-				capacity=0;
-				buffer=new T[capacity]
-				}
-			Vector(const Vector &asv){
-				int i;
-				capacity=asv.getCapacity();
-				buffer=new T[asv.getCapacity()];
-			        for (i=0; i<capacity; i++){
-					buffer[i]=asv[i];
-				}
+			typedef T* iterator;
+			typedef T value_type;
+
+			Vector()
+			{
+				_size = 0;
+				_capacity = 0;
+				_data = 0;
 			}
-			~Vector(){
-				delete []buffer;
+			~Vector()
+			{
+				deleteRange(_data, _data + _size);
+				free(_data);
 			}
-			void push_back(T_obj){
-				Vector oldSV(*this)
+//			Vector(const Vector &asv){
+//				int i;
+//				capacity=asv.getCapacity();
+//				buffer=new T[asv.getCapacity()];
+//			        for (i=0; i<capacity; i++){
+//					buffer[i]=asv[i];
+//				}
+//			~Vector(){
+//			
+//				delete []buffer;
+			void push_back(const T& value)
+			{
+					
+			}		
+			void pop_back()
+			{
+			
+			}
+			void erase()
+			{
+			
 			}
 	}
